6859adcb392723408bac667e32e653896ede6b02
Eren Uyğun <eren.uygun.2657@gmail.com>
2021-11-30 23:18:44 +0300

    InvoiceDao

--- ReCapProject/src/main/java/com/etiya/ReCapProject/dataAccess/abstracts/InvoiceDao.java
+++ ReCapProject/src/main/java/com/etiya/ReCapProject/dataAccess/abstracts/InvoiceDao.java
@@ -1,16 +1,23 @@
 package com.etiya.ReCapProject.dataAccess.abstracts;
 
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.etiya.ReCapProject.business.dtos.InvoiceSearchListDto;
-import com.etiya.ReCapProject.core.utilities.results.DataResult;
 import com.etiya.ReCapProject.entities.concretes.Invoice;
+import org.springframework.data.jpa.repository.Query;
 
 public interface InvoiceDao extends JpaRepository<Invoice, Integer>{
 	
 	List<InvoiceSearchListDto> getByCustomer_Id(int id);
+
+	@Query("select i from Invoice i where i.createDate between ?1 and ?2")
+	List<InvoiceSearchListDto> getByCreateDateBetween(LocalDate beginDate, LocalDate endDate);
+	
+	
+	
 
 }
