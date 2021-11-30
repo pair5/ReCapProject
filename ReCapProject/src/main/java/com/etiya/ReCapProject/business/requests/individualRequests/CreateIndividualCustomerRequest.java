5a3066d4cab575342100ce19ef0ca780c5289314
Eren Uyğun <eren.uygun.2657@gmail.com>
2021-11-30 16:47:17 +0300

    birthdate

--- ReCapProject/src/main/java/com/etiya/ReCapProject/business/requests/individualRequests/CreateIndividualCustomerRequest.java
+++ ReCapProject/src/main/java/com/etiya/ReCapProject/business/requests/individualRequests/CreateIndividualCustomerRequest.java
@@ -17,7 +17,7 @@ public class CreateIndividualCustomerRequest {
 	private int id;
 	private String firstName;
 	private String lastName;
-	private LocalDate birthDate;
+	private LocalDate birthdate;
 	private String email;
 	private String password;
 }
