7d25d6ee1be2f8253c61d05f1da2f7fbf242de60
Eren Uyğun <eren.uygun.2657@gmail.com>
2021-11-30 17:05:46 +0300

    Corpo Corpo Corpo

--- ReCapProject/src/main/java/com/etiya/ReCapProject/dataAccess/abstracts/CorporateCustomerDao.java
+++ ReCapProject/src/main/java/com/etiya/ReCapProject/dataAccess/abstracts/CorporateCustomerDao.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.etiya.ReCapProject.entities.concretes.CorporateCustomer;
 
 public interface CorporateCustomerDao extends JpaRepository<CorporateCustomer, Integer> {
+	
+	boolean existsByEmail(String email);
 
 }
